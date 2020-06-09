35be0b74fb3eb647ed4f968ae92a198d0216510e
Max Shonichev <mshonichev@gridgain.com>
2020-06-09 11:00:22 +0300

    Licenses and fixes (#1)
    * added licenses
    * refactor `Gatling` to use `JavaApp`

--- plugins/mavenbuild.py
+++ plugins/mavenbuild.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
+#
+# Copyright 2017-2020 GridGain Systems.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 from tiden.tidenplugin import TidenPlugin
 from tiden.util import log_print
@@ -8,11 +22,14 @@ from os import system, getcwd, chdir, unlink, walk
 from os.path import dirname, abspath, join, basename, getmtime
 from datetime import datetime
 from glob import glob
+from re import match
 
 TIDEN_PLUGIN_VERSION = '1.0.0'
 
 
 class MavenBuild(TidenPlugin):
+    # pattern of file names ignored when detecting source changes
+    ignore_files = r'^(\.git.*|.*\.log)$'
 
     mvn_command = 'mvn'
     always_rebuild = False
@@ -41,7 +58,11 @@ class MavenBuild(TidenPlugin):
                     if 'build_path' not in artifact['mvn']:
                         raise TidenException("mvn section of artifact must have 'build_path' attribute")
                     build_path = artifact['mvn']['build_path']
-                    if self._need_rebuild(artifact, build_path):
+                    if self._need_rebuild(
+                            artifact,
+                            build_path,
+                            artifact['mvn'].get('ignore_files', MavenBuild.ignore_files)
+                    ):
                         mvn_args = artifact['mvn'].get('mvn_args', '')
                         if not self._build_maven_artifact(artifact_name, build_path, mvn_args):
                             build_log = basename(self._get_maven_build_log_name())
@@ -69,16 +90,35 @@ class MavenBuild(TidenPlugin):
             chdir(current_directory)
         return rc == 0
 
-    def _need_rebuild(self, artifact, build_path):
+    def _need_rebuild(self, artifact, build_path, ignore_files):
         if self.always_rebuild:
             return True
         if 'always_rebuild' in artifact['mvn'] and bool(artifact['mvn']['always_rebuild']):
             return True
-        artifact_jar = glob(artifact['glob_path'])
-        if not artifact_jar:
+        artifact_jars = glob(artifact['glob_path'])
+        if not artifact_jars:
             return True
 
-        files_stat = {}
+        max_jar_mtime = 0
+        for artifact_jar in artifact_jars:
+            cur_jar_mtime = getmtime(artifact_jar)
+            if cur_jar_mtime > max_jar_mtime:
+                max_jar_mtime = cur_jar_mtime
+
+        max_mtime = 0
         for root, dirs, files in walk(build_path):
+            if root != build_path:
+                if root.startswith(join(build_path, 'target')):
+                    continue
+
             for file in files:
-                files_stat[file] = getmtime(join(root, file))
+                if match(ignore_files, file):
+                    continue
+                cur_mtime = getmtime(join(root, file))
+                if cur_mtime > max_mtime:
+                    max_mtime = cur_mtime
+                    if max_mtime > max_jar_mtime:
+                        return True
+
+        return False
+
--- suites/examples/test_repeated_test.py
+++ suites/examples/test_repeated_test.py
@@ -1,3 +1,19 @@
+#!/usr/bin/env python3
+#
+# Copyright 2017-2020 GridGain Systems.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # !/usr/bin/env python3
 
 from tiden.case.generaltestcase import GeneralTestCase
@@ -34,3 +50,4 @@ class TestRepeatedTest(GeneralTestCase):
                                           self.config['rt']['test_class'])])
         dir_list = ''.join(list(result.values())[0]).splitlines()
         assert set(dir_list) == set(self.pass_test_dir), 'assert list of test dirs on server and expected are equal'
+
